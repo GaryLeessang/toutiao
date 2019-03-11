1519f3101555e8572bcd433c75b8f833439de92c
Gary <292305444@qq.com>
2019-03-11 13:27:32 +0800

    modify the news.ftl

--- src/main/java/com/example/toutiao/controller/IndexController.java
+++ src/main/java/com/example/toutiao/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.example.toutiao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by gary on 2019-03-10
@@ -28,7 +34,16 @@ public class IndexController {
     }
 
     @RequestMapping(path = {"/ftl"})
-    public String news() {
+    public String news(Model model) {
+        model.addAttribute("value1", "This is a trial");
+        List<String> colors = Arrays.asList(new String[] {"Red", "Green", "Blue"});
+        model.addAttribute("colors", colors);
+
+        Map<String, String> mapping = new HashMap<String, String>();
+        for(int i=0; i<4; i++){
+            mapping.put(String.valueOf(i), String.valueOf(i*i));
+        }
+        model.addAttribute("map", mapping);
         return "news";
     }
 }
--- src/main/resources/templates/news.ftl
+++ src/main/resources/templates/news.ftl
@@ -2,6 +2,20 @@
 <body>
 <pre>
     Hello freemarker!
+
+    <#-- You can not see me -->
+    ${value1}
+    ${value2!}
+    <#list colors as color>
+        ${color_index} -- ${color}
+        <#if color = "Green">Green will stop this loop! Be careful!<#break/></#if>
+    </#list>
+
+    <#list map as key, value>
+        ${key} = ${value}
+    </#list>
+
+
 </pre>
 </body>
 </html>
